43fa177af23fd3f4eeb62a16429fbe8a60fca2d4
Stablaze <1350956280@qq.com>
2023-10-29 17:48:27 +0800

    Update Gemstone weapon family

---
+++ src/main/java/cn/bzlom/lanthanum/registry/ModToolMaterials.java
@@ -0,0 +1,53 @@
+package cn.bzlom.lanthanum.registry;
+
+import net.minecraft.item.ToolMaterial;
+import net.minecraft.recipe.Ingredient;
+import net.minecraft.util.Lazy;
+
+import java.util.function.Supplier;
+
+public enum ModToolMaterials implements ToolMaterial {
+    GEMSTONE(2, 150, 14.0F, 3F, 22, () -> {
+        return Ingredient.ofItems(ModItems.GEMSTONE);
+    });
+
+    private final int miningLevel;
+    private final int itemDurability;
+    private final float miningSpeed;
+    private final float attackDamage;
+    private final int enchantability;
+    private final Lazy<Ingredient> repairIngredient;
+
+    private ModToolMaterials(int miningLevel, int itemDurability, float miningSpeed, float attackDamage, int enchantability, Supplier repairIngredient) {
+        this.miningLevel = miningLevel;
+        this.itemDurability = itemDurability;
+        this.miningSpeed = miningSpeed;
+        this.attackDamage = attackDamage;
+        this.enchantability = enchantability;
+        this.repairIngredient = new Lazy(repairIngredient);
+    }
+
+    public int getDurability() {
+        return this.itemDurability;
+    }
+
+    public float getMiningSpeedMultiplier() {
+        return this.miningSpeed;
+    }
+
+    public float getAttackDamage() {
+        return this.attackDamage;
+    }
+
+    public int getMiningLevel() {
+        return this.miningLevel;
+    }
+
+    public int getEnchantability() {
+        return this.enchantability;
+    }
+
+    public Ingredient getRepairIngredient() {
+        return (Ingredient)this.repairIngredient.get();
+    }
+}
