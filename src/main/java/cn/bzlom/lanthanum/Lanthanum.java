43f2f6a43cb9593724c7b5cacc8698de6d5ca96b
lingyunmo <96475062+lingyunmo@users.noreply.github.com>
2023-10-29 18:17:23 +0800

    Revert "Update the craft and smelt of gemstone and the generation of gemstone"

--- src/main/java/cn/bzlom/lanthanum/Lanthanum.java
+++ src/main/java/cn/bzlom/lanthanum/Lanthanum.java
@@ -4,7 +4,6 @@ import cn.bzlom.lanthanum.item.LanthanumFoodComponents;
 import cn.bzlom.lanthanum.registry.ModBlocks;
 import cn.bzlom.lanthanum.registry.ModItemGroup;
 import cn.bzlom.lanthanum.registry.ModItems;
-import cn.bzlom.lanthanum.registry.ModOreGen;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
@@ -31,6 +30,5 @@ public class Lanthanum implements ModInitializer {
         ModItemGroup.registerModItemGroup();
         ModItems.registerItems();
         ModBlocks.registerModBlocks();
-        ModOreGen.generateOres();
     }
 }
