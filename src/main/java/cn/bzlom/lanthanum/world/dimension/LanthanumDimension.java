9686fb3183edb57b8f1006ad5295bddd1b122524
shenlingzhi <2087789639@qq.com>
2023-10-30 09:18:13 +0800

    Update the version and a new dimension without river and ocean

---
+++ src/main/java/cn/bzlom/lanthanum/world/dimension/LanthanumDimension.java
@@ -0,0 +1,14 @@
+package cn.bzlom.lanthanum.world.dimension;
+
+import net.minecraft.registry.RegistryKey;
+import net.minecraft.registry.RegistryKeys;
+import net.minecraft.util.Identifier;
+import net.minecraft.world.World;
+import net.minecraft.world.dimension.DimensionType;
+
+public class LanthanumDimension {
+    public static final RegistryKey<World> LANTHANUM_DIMENSION_KEY = RegistryKey.of(RegistryKeys.WORLD,
+            new Identifier("lanthanum","lanthanum_dimension"));
+    public static final RegistryKey<DimensionType> LANTHANUM_DIMENSION_TYPE_KEY = RegistryKey.of(RegistryKeys.DIMENSION_TYPE,
+            new Identifier("lanthanum","lanthanum_dimension_type"));
+}
