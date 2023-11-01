dad9a72878fb77022fd8073e84f3a826d21c78d9
lingyunmo <2087789639@qq.com>
2023-11-01 18:06:51 +0800

    Add lanthanum tools and the Ore generation in nature and crafting the block

---
+++ src/main/java/cn/bzlom/lanthanum/registry/ModFoods.java
@@ -0,0 +1,4 @@
+package cn.bzlom.lanthanum.registry;
+
+public class ModFoods {
+}
--- src/main/java/cn/bzlom/lanthanum/registry/ModOreGen.java
+++ src/main/java/cn/bzlom/lanthanum/registry/ModOreGen.java
@@ -6,8 +6,14 @@ import net.minecraft.world.gen.GenerationStep;
 
 public class ModOreGen {
     public static void generateOres() {
+        //Gemstone
         BiomeModifications.addFeature(BiomeSelectors.foundInOverworld(), GenerationStep.Feature.UNDERGROUND_ORES, ModOrePlacedFeatures.GEMSTONE_ORE_MIDDLE);
         BiomeModifications.addFeature(BiomeSelectors.foundInOverworld(), GenerationStep.Feature.UNDERGROUND_ORES, ModOrePlacedFeatures.GEMSTONE_ORE_SMALL);
         BiomeModifications.addFeature(BiomeSelectors.foundInOverworld(), GenerationStep.Feature.UNDERGROUND_ORES, ModOrePlacedFeatures.GEMSTONE_ORE_UPPER);
+
+        //Lanthanum
+        BiomeModifications.addFeature(BiomeSelectors.foundInOverworld(), GenerationStep.Feature.UNDERGROUND_ORES, ModOrePlacedFeatures.LANTHANUM_ORE_MIDDLE);
+        BiomeModifications.addFeature(BiomeSelectors.foundInOverworld(), GenerationStep.Feature.UNDERGROUND_ORES, ModOrePlacedFeatures.LANTHANUM_ORE_SMALL);
+        BiomeModifications.addFeature(BiomeSelectors.foundInOverworld(), GenerationStep.Feature.UNDERGROUND_ORES, ModOrePlacedFeatures.LANTHANUM_ORE_UPPER);
     }
 }
