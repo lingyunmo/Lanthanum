026fcd81fa845747a1479db66aaf4082e8e22ec5
lingyunmo <2087789639@qq.com>
2023-11-06 19:43:18 +0800

    Fix bug caused by AI

--- src/main/java/cn/bzlom/lanthanum/utils/ThirstData.java
+++ src/main/java/cn/bzlom/lanthanum/utils/ThirstData.java
@@ -8,27 +8,29 @@ import net.minecraft.network.PacketByteBuf;
 import net.minecraft.server.network.ServerPlayerEntity;
 
 public class ThirstData {
-    public static int addThirst(IEntityDataSaver player, int amount){
+    public static int addThirst(IEntityDataSaver player, int amount) {
         NbtCompound nbt = player.getPersistentData();
         int thirst = nbt.getInt("thirst");
-        thirst = Math.min(thirst + amount,10);
-        nbt.putInt("thirst",thirst);
-+       syncThirst(thirst,(ServerPlayerEntity) player);
+        thirst = Math.min(thirst + amount, 10);
+        nbt.putInt("thirst", thirst);
+        if (player instanceof ServerPlayerEntity) {
+            syncThirst(thirst, (ServerPlayerEntity) player);
+        }
         return thirst;
     }
 
-    public static int removeThirst(IEntityDataSaver player,int amount){
+    public static int removeThirst(IEntityDataSaver player, int amount) {
         NbtCompound nbt = player.getPersistentData();
         int thirst = nbt.getInt("thirst");
-        thirst = Math.max(0,thirst - amount);
-        nbt.putInt("thirst",thirst);
-        syncThirst(thirst,(ServerPlayerEntity) player);
+        thirst = Math.max(0, thirst - amount);
+        nbt.putInt("thirst", thirst);
+        syncThirst(thirst, (ServerPlayerEntity) player);
         return thirst;
     }
 
-    public static void syncThirst(int thirst, ServerPlayerEntity player){
+    public static void syncThirst(int thirst, ServerPlayerEntity player) {
         PacketByteBuf buf = PacketByteBufs.create();
         buf.writeInt(thirst);
-        ServerPlayNetworking.send(player, ModMessage.THIRST_SYNC_ID,buf);
+        ServerPlayNetworking.send(player, ModMessage.THIRST_SYNC_ID, buf);
     }
 }
